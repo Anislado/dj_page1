ff31548045cd786c1723b1c51c358a10b3579e85
Anislado <mauriciogonzalez36@aragon.unam.mx>
2026-05-22 18:16:59 -0600

    catalogo3

--- src/Catalogo.jsx
+++ src/Catalogo.jsx
@@ -414,40 +414,8 @@ export default function Catalogo() {
         {/* BOTON */}
         <button
           className="cotizar-btn w-100 mt-4"
-          onClick={() => {
+            onClick={() => addToCart(product)}
 
-            const cart = JSON.parse(
-              localStorage.getItem("cart")
-            ) || [];
-
-            const existingProduct = cart.find(
-              (item) =>
-                item._id === product._id
-            );
-
-            if (existingProduct) {
-
-              existingProduct.quantity += 1;
-
-            } else {
-
-              cart.push({
-                ...product,
-                quantity: 1,
-              });
-
-            }
-
-            localStorage.setItem(
-              "cart",
-              JSON.stringify(cart)
-            );
-
-            alert(
-              "Producto agregado al carrito"
-            );
-
-          }}
         >
 
           Agregar al carrito
